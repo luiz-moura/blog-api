d53db7f4edac5f89b10b80b6bb8a5032945380c8
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-04-29 16:00:24 -0400

    :sparkles: create companySeeder

---
+++ app/Database/Seeds/CompanySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Company extends Seeder
+{
+	public function run()
+	{
+		$model = model('CompanyModel');
+
+    $data = [
+      [
+        'name'          => 'AnyCode',
+        'description'   => 'AnyCode description',
+        'email'         => 'anycodesuport@gmail.com',
+      ],
+    ];
+
+    $model->insertBatch($data);
+	}
+}
