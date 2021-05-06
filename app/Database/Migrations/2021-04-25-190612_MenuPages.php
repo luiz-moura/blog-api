ae1ce0cc772d4c94e39c65db539559b537ca6b49
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-05-06 15:23:26 -0400

    :zap: permity null in field parent MenuPage

--- app/Database/Migrations/2021-04-25-190612_MenuPages.php
+++ app/Database/Migrations/2021-04-25-190612_MenuPages.php
@@ -24,6 +24,7 @@ class MenuPages extends Migration
 				'type'           		=> 'INT',
 				'constraint'     		=> 5,
 				'unsigned'       		=> true,
+        'null'              => true,
 			],
       'title'					    => [
 				'type'           		=>'VARCHAR',
